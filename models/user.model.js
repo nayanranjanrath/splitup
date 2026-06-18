0fd612a0bf39ca150ccb4b09587ea2bd267f977d
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-06-18 07:08:31 +0530

    created some methods and utility for bcrypt and access token and refresh token

--- models/user.model.js
+++ models/user.model.js
@@ -49,7 +49,23 @@ const userSchema = new schema({
 })
 
 
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password,10)
+    }
+    return 
+})
 
+userSchema.methods.ispasswordcorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.createaccesstoken=function(){
+    return jwt.sign({_id:this._id},process.env.ACCESSTOKEN_SECRET,{expiresIn:ACCESSTOKEN_EXP})
+}
+userSchema.methods.createrefreshtoken=function(){
+    return jwt.sign({_id:this._id},process.env.REFRESHTOKEN_SECRET,{expiresIn:REFRESHTOKEN_EXP})
+}
 const usermodel = new model("user",userSchema)
 
 export default usermodel
